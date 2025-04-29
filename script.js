6eb81fcf2de6df1a0e09b9ce557da985d0be5a55
ifty <ifty.alamin54@gmail.com>
2025-04-29 12:39:17 +0600

    reverted back

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 function createCounter() {
-  let count = 0;
-
-  return {
-    increment: function () {
-      console.log("Increment clicked. Waiting 2 seconds...");
-      setTimeout(() => {
-        count++;
+    let count = 0;
+  
+    return {
+      increment: function() {
+        console.log("Increment clicked. Waiting 2 seconds...");
+        setTimeout(() => {
+          count++;
+          updateDisplay();
+          console.log("Incremented!");
+        }, 2000);
+      },
+      decrement: function() {
+        count--;
         updateDisplay();
-        console.log("Incremented!");
-      }, 2000);
-    },
-    decrement: function () {
-      count--;
-      updateDisplay();
-      console.log("Decremented immediately!");
-    },
-    reset: function () {
-      count = 0;
-      updateDisplay();
-      console.log("Reset done!");
-    },
-    getCount: function () {
-      return count;
-    },
-  };
-
-  function updateDisplay() {
-    const el = document.getElementById("counter");
-    if (el) el.innerText = count;
+        console.log("Decremented immediately!");
+      },
+      reset: function() {
+        count = 0;
+        updateDisplay();
+        console.log("Reset done!");
+      },
+      getCount: function() {
+        return count;
+      }
+    };
+  
+    function updateDisplay() {
+      document.getElementById("counter").innerText = count;
+    }
   }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
+  
   const counter = createCounter();
-
+  
   document.getElementById("inc").addEventListener("click", counter.increment);
   document.getElementById("dec").addEventListener("click", counter.decrement);
   document.getElementById("reset").addEventListener("click", counter.reset);
-});
+  

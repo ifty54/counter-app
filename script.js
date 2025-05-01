ef1a068aa11fecc9e79740fcc0281140c34871bb
ifty <ifty.alamin54@gmail.com>
2025-05-01 21:15:20 +0600

    navbar extendedly modified

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 function createCounter() {
-    let count = 0;
-  
-    return {
-      increment: function() {
-        console.log("Increment clicked. Waiting 2 seconds...");
-        setTimeout(() => {
-          count++;
-          updateDisplay();
-          console.log("Incremented!");
-        }, 2000);
-      },
-      decrement: function() {
-        count--;
+  let count = 0;
+
+  return {
+    increment: function () {
+      console.log("Increment clicked. Waiting 2 seconds...");
+      setTimeout(() => {
+        count++;
         updateDisplay();
-        console.log("Decremented immediately!");
-      },
-      reset: function() {
-        count = 0;
-        updateDisplay();
-        console.log("Reset done!");
-      },
-      getCount: function() {
-        return count;
-      }
-    };
-  
-    function updateDisplay() {
-      document.getElementById("counter").innerText = count;
-    }
+        console.log("Incremented!");
+      }, 2000);
+    },
+    decrement: function () {
+      count--;
+      updateDisplay();
+      console.log("Decremented immediately!");
+    },
+    reset: function () {
+      count = 0;
+      updateDisplay();
+      console.log("Reset done!");
+    },
+    getCount: function () {
+      return count;
+    },
+  };
+
+  function updateDisplay() {
+    document.getElementById("counter").innerText = count;
   }
-  
-  const counter = createCounter();
-  
-  document.getElementById("inc").addEventListener("click", counter.increment);
-  document.getElementById("dec").addEventListener("click", counter.decrement);
-  document.getElementById("reset").addEventListener("click", counter.reset);
-  
+}
+
+const counter = createCounter();
+
+document.getElementById("inc").addEventListener("click", counter.increment);
+document.getElementById("dec").addEventListener("click", counter.decrement);
+document.getElementById("reset").addEventListener("click", counter.reset);
+
+// Corrected toggling logic
+let toggleState = true;
+setInterval(() => {
+  if (toggleState) {
+    document.getElementById("brandText").style.display = "inline";
+    document.getElementById("brandImg").style.display = "none";
+  } else {
+    document.getElementById("brandText").style.display = "none";
+    document.getElementById("brandImg").style.display = "inline";
+  }
+  toggleState = !toggleState;
+}, 2000);
